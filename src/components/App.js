507fdfca9914b128c8206433ffd9ce97a86d5df8
ismail harmanda <iharmanda@hotmail.com>
2021-07-10 22:02:32 +0300

    middle container implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/App.css"
+import MiddleContainer from "./facilities/middleContainer";
 import FirstPage from "./FirstPage"
 import SecondPage from "./SecondPage"
 const App = ()=>{
     return(
     <div>
         <FirstPage />
+        <MiddleContainer />
         <SecondPage />
     </div>
     )
---
+++ src/components/facilities/middleContainer.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const MiddleContainer = ()=>{
+    return(
+        <div
+        style={{
+            background:"#0052CC",
+            height:"132px",
+            borderRadius:"10px",
+            position:"absolute",
+            left: "0",
+  right: "0", 
+  marginLeft: "auto", 
+  marginRight: "auto"
+            
+        }}
+         className="container">
+
+        </div>
+    )
+}
+
+export default MiddleContainer
