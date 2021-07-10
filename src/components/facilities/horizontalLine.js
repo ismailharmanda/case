eb0e56e772239d4705713ea0393ff3d872ef7ee4
ismail harmanda <iharmanda@hotmail.com>
2021-07-10 16:41:22 +0300

    HorizontalLine facility fnished

---
+++ src/components/facilities/horizontalLine.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const HorizontalLine= ()=>{
+    return(
+        <div style={{
+width: "230px",
+height: "10px",
+background:"#0052CC"
+        }}>
+        </div>
+    )
+}
+export default HorizontalLine;
