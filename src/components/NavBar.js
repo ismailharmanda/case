78e3880255c0788967e12d48c6e588e53bc7a4b0
ismail harmanda <iharmanda@hotmail.com>
2021-07-11 20:11:07 +0300

    a tag added to navbar social media icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,43 +3,71 @@ import Logo from "../images/Logo";
 import Facebook from "../images/Facebook";
 import Twitter from "../images/Twitter";
 import Instagram from "../images/Instagram";
-import "../styles/Navbar.css"
-const NavBar=()=>{
-    return (
-<div className="roboto">
-<nav className="navbar navbar-light bg-white">
-<div className="container-fluid p-0 m-0 d-flex justify-content-between align-items-center">
-        <div className="pt-2">
+import "../styles/Navbar.css";
+const NavBar = () => {
+  return (
+    <div className="roboto">
+      <nav className="navbar navbar-light bg-white">
+        <div className="container-fluid p-0 m-0 d-flex justify-content-between align-items-center">
+          <div className="pt-2">
             <a className="navbar-brand" href="/#">
-      <Logo />
+              <Logo />
             </a>
-        </div>
-        <div className="pt-3">
+          </div>
+          <div className="pt-3">
             <ul className="">
-                <li className="nav-item d-inline mx-2 ">
-                    <a className="grayColor" href="/#">Home</a>
-                </li>
-                <li className="nav-item d-inline mx-2">
-                    <a className="grayColor" href="/#">Services</a>
-                </li>
-                <li className="nav-item d-inline mx-2">
-                    <a className="grayColor" href="/#">Downloads</a>
-                </li>
-                <li className="nav-item d-inline mx-2">
-                    <a className="grayColor" href="/#">Contact us</a>
-                </li>
+              <li className="nav-item d-inline mx-2 ">
+                <a className="grayColor" href="/#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item d-inline mx-2">
+                <a className="grayColor" href="/#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item d-inline mx-2">
+                <a className="grayColor" href="/#">
+                  Downloads
+                </a>
+              </li>
+              <li className="nav-item d-inline mx-2">
+                <a className="grayColor" href="/#">
+                  Contact us
+                </a>
+              </li>
             </ul>
-      </div>
-      <div className="pt-0">
-                <Facebook />
-                <Twitter />
-                <Instagram />
-    </div>
-</div>
-</nav>
+          </div>
+          <div className="pt-0">
+            <a
+              href="/#"
+              style={{
+                textDecoration: "none",
+              }}
+            >
+              <Facebook />
+            </a>
+            <a
+              href="/#"
+              style={{
+                textDecoration: "none",
+              }}
+            >
+              <Twitter />
+            </a>
+            <a
+              href="/#"
+              style={{
+                textDecoration: "none",
+              }}
+            >
+              <Instagram />
+            </a>
+          </div>
         </div>
-    )
-
-}
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
