f42c01461c88f8d46dde109f132dd1e4e1d93e30
ismail harmanda <iharmanda@hotmail.com>
2021-07-11 17:09:55 +0300

    Forth page done

--- src/components/Subcribe.js
+++ src/components/Subcribe.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./facilities/button";
 
 const Subcribe = () => {
   return (
@@ -7,7 +8,52 @@ const Subcribe = () => {
         background: "rgba(0, 82, 204, 0.2)",
       }}
       className="col-12 col-md-6 p-5 mt-5"
-    ></div>
+    >
+      <div className="float-md-left p-5 mt-5 ml-5">
+        <h6
+          className="mb-5"
+          style={{
+            fontFamily: "Roboto",
+            fontWeight: "500",
+            fontSize: "12px",
+            color: "#5B6B88",
+          }}
+        >
+          SHORT HEADLINE
+        </h6>
+        <h2
+          className="mb-5 pt-4"
+          style={{
+            fontFamily: "Roboto",
+            fontWeight: "700",
+            fontSize: "60px",
+            color: "#5B6B88",
+          }}
+        >
+          Subcribe our <br /> newsletter
+        </h2>
+        <p
+          className="mb-5 pt-1"
+          style={{
+            fontFamily: "Roboto",
+            fontWeight: "400",
+            fontSize: "20px",
+            color: "#5B6B88",
+          }}
+        >
+          Online businesses utilize newsletters to <br /> keep their brand
+          top-of-mind for <br /> consumers, establishing authority.
+        </p>
+        <Button
+          width="230px"
+          height="50px"
+          borderRadius="8px"
+          background="#0052CC"
+          color="white"
+          text="Learn More"
+        />
+      </div>
+    </div>
   );
 };
 
